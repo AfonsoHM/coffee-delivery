b0b0cd25008b655b4a39a255ec56ebdcd4fca9cb
Afonso Monteiro <affonsoh.monteiro@gmail.com>
2025-02-14 22:23:55 -0300

    created component AddressForm

--- src/pages/CompleteOrder/components/CompleteOrderForm/index.tsx
+++ src/pages/CompleteOrder/components/CompleteOrderForm/index.tsx
@@ -1,7 +1,8 @@
-import { MapPinLine } from "phosphor-react";
+import { CurrencyDollar, MapPinLine } from "phosphor-react";
 import { SectionTitle } from "../SectionTitle";
 import { CompleteOrderFormContainer, FormSectionContainer, Title } from "./styles";
 import { useTheme } from "styled-components";
+import { AddressForm } from "./AddressForm";
 
 export function CompleteOrderForm() {
   const { colors} = useTheme()
@@ -18,6 +19,18 @@ export function CompleteOrderForm() {
           subtitle="Informe o endereço onde deseja receber seu pedido"
           icon={<MapPinLine size={22} color={colors["brand-yellow-dark"]} />}
         />
+
+        <AddressForm />
+      </FormSectionContainer>
+
+      <FormSectionContainer>
+        <SectionTitle 
+          title="Pagamento"
+          subtitle="O pagamento é feito na entrega. Escolha a forma que deseja pagar"
+          icon={<CurrencyDollar size={22} color={colors["brand-purple"]} />}
+        />
+
+      
       </FormSectionContainer>
     </CompleteOrderFormContainer>
   )
